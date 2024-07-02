0badfe4ccab7176a323fa1162e03519185970d68
harshitchopra7 <chopraharshit2000@gmail.com>
2024-07-02 10:24:13 +0530

    server created

---
+++ index.js
@@ -0,0 +1,11 @@
+const add = require("./calculate.js");
+
+console.log(add(10, 20));
+
+// what action do you want to perform?
+
+// 1. Addition
+// 2. Subtraction
+// 3. Multiplication
+// 4. Division
+// 5. Percentage
---
+++ server.js
@@ -0,0 +1,12 @@
+// http
+const http = require("http");
+
+// 1. create server
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/html");
+  res.end("<h1>Maggu</h1>");
+});
+
+// 2. running the created server
+server.listen(3000);
